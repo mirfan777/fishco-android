70a0f59c69e755ae2f8d4766373782a67ec4a7e3
zollahrp <141205140+zollahrp@users.noreply.github.com>
2024-11-17 16:46:38 +0700

    intent homepage

--- app/src/main/java/com/example/fishco/activity/home/HomepageActivity.java
+++ app/src/main/java/com/example/fishco/activity/home/HomepageActivity.java
@@ -1,6 +1,11 @@
 package com.example.fishco.activity.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.fishco.R;
+import com.example.fishco.activity.article.ArticleDetailActivity;
+import com.example.fishco.activity.encyclopedia.FishListActivity;
+import com.example.fishco.activity.settings.ProfileActivity;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -17,10 +25,61 @@ public class HomepageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_homepage);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Intent untuk navigasi ke ProfileActivity
+        ImageView profilePicture = findViewById(R.id.profile_picture);
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomepageActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+        FrameLayout frameLayout = findViewById(R.id.frame_layout_article);
+        frameLayout.setOnClickListener(view -> {
+            Intent intent = new Intent(HomepageActivity.this, ArticleDetailActivity.class);
+            startActivity(intent);
+        });
+        // Freshwater Category
+        LinearLayout categoryFreshwater = findViewById(R.id.category_freshwater);
+        categoryFreshwater.setOnClickListener(v -> {
+            Intent intent = new Intent(HomepageActivity.this, FishListActivity.class);
+            intent.putExtra("CATEGORY_NAME", "Freshwater Fish");
+            startActivity(intent);
+        });
+        // Brackfish Category
+        LinearLayout categoryMarine = findViewById(R.id.category_marine);
+        categoryMarine.setOnClickListener(v -> {
+            Intent intent = new Intent(HomepageActivity.this, FishListActivity.class);
+            intent.putExtra("CATEGORY_NAME", "Marine Fish");
+            startActivity(intent);
+        });
+        // Invertebrates Category
+        LinearLayout categoryInvertebrates = findViewById(R.id.category_invertebrates);
+        categoryInvertebrates.setOnClickListener(v -> {
+            Intent intent = new Intent(HomepageActivity.this, FishListActivity.class);
+            intent.putExtra("CATEGORY_NAME", "Freshwater Fish");
+            startActivity(intent);
+        });
+        // Coldwater Category
+        LinearLayout categoryColdwater = findViewById(R.id.category_coldwater);
+        categoryColdwater.setOnClickListener(v -> {
+            Intent intent = new Intent(HomepageActivity.this, FishListActivity.class);
+            intent.putExtra("CATEGORY_NAME", "Freshwater Fish");
+            startActivity(intent);
+        });
+        // Predatory Category
+        LinearLayout categoryPredatory = findViewById(R.id.category_predatory);
+        categoryPredatory.setOnClickListener(v -> {
+            Intent intent = new Intent(HomepageActivity.this, FishListActivity.class);
+            intent.putExtra("CATEGORY_NAME", "Freshwater Fish");
+            startActivity(intent);
         });
     }
 }
